f37f6a9b5113fda91582871808e0a2dfcb9d2532
Vasyl Hrubyi <ronaldomen7@gmail.com>
2025-11-16 18:00:24 +0200

    fix-mistakes

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
-import type { MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3";
+
+
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export async function fetchMovies(
   query: string,
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -8,9 +8,3 @@
     vote_average: number;
 }
 
-export interface MoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
